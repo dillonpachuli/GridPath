346228956ee42f47c5bee24a85fbdd7a99379929
Dillon Chapuli <148895731+dillonpachuli@users.noreply.github.com>
2024-11-18 17:31:03 +0000

    11.18.24 - FINAL

--- GridPath.java
+++ GridPath.java
@@ -34,8 +34,10 @@ public class GridPath {
      * grid.
      */
     public Location getNextLoc(int row, int col) {
-        int below = grid[row+1][col];
-        int right = grid[row][col+1];
+        int below = 100;
+        if (row < grid.length - 1) below = grid[row+1][col];
+        int right = 100;
+        if (col < grid[0].length - 1) right = grid[row][col+1];
         if (below < right) return new Location(row+1, col);
         else return new Location (row, col+1);
     }
@@ -51,5 +53,14 @@ public class GridPath {
      * grid.
      */
     public int sumPath(int row, int col) {
-        /* to be implemented in part (b) */ }
+        int sum = grid[row][col];
+        while (row < grid.length - 1 || col < grid[0].length - 1)
+        {
+            Location loc = getNextLoc(row, col);
+            row = loc.getRow();
+            col = loc.getCol();
+            sum += grid[row][col];
+        }
+        return sum;
+    }
 }
--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int[][] nums =
+        int[][] gridOne =
         {
             {12, 3, 4, 13, 5},
             {11, 21, 2, 14, 16},
@@ -10,8 +10,24 @@ public class Main
             {10, 17, 20, 19, 1},
             {18, 22, 30, 25, 6}
         };
-        GridPath one = new GridPath(nums);
+        GridPath one = new GridPath(gridOne);
         System.out.println(one);
         System.out.println(one.getNextLoc(0, 0));
+        System.out.println(one.getNextLoc(1, 3));
+        System.out.println(one.getNextLoc(2, 4));
+        System.out.println(one.getNextLoc(4, 3));
+        System.out.println("");
+        System.out.println("");
+        
+        int[][] gridTwo =
+        {
+            {12, 30, 40, 25, 5},
+            {11, 3, 22, 15, 43},
+            {7, 2, 9, 4, 0},
+            {8, 33, 18, 6, 1}
+        };
+        GridPath two = new GridPath(gridTwo);
+        System.out.println(two);
+        System.out.println(two.sumPath(0, 0));
     }
 }
